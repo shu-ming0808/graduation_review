c72135cf9b303a6682333e6888bd394f3c7f9db0
shu-ming0808 <sammy151516@gmail.com>
2026-06-11 13:31:47 +0800

    擴容從最大同時 DB 連線約 15 條到150 條

--- backend/database.py
+++ backend/database.py
@@ -4,14 +4,18 @@ from sqlalchemy.orm import sessionmaker
 DB_HOST = "localhost"
 DB_PORT = 3306
 DB_USER = "root"
-DB_PASSWORD = "nccutest"
+DB_PASSWORD = "910808"
 DB_NAME = "mydb"
 
 DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(
     DATABASE_URL,
-    echo=True
+    echo=False,
+    pool_size=50,
+    max_overflow=100,
+    pool_timeout=30,
+    pool_pre_ping=True,
 )
 
 SessionLocal = sessionmaker(
